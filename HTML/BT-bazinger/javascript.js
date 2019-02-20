730520a92a3b5d48490e1e7f6369d0fc2316559c
NgocLan <nguyenthilan24497@gmail.com>
2019-02-20 11:00:07 +0700

    up bazinger

--- HTML/BT-bazinger/javascript.js
+++ HTML/BT-bazinger/javascript.js
@@ -9,3 +9,12 @@ function toggle(){
     }
     document.getElementById('about-contact').style.display = style;
 }
+$(document).ready(function() {
+    $("a[href*='#']:not([href='#])").click(function() {
+        let target = $(this).attr("href");
+        $('html,body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 1000);
+        event.preventDefault();
+    });
+});
